49dcd5dfe1021886108a5f96ae50085b63dad633
Andrea Gradassi <82960682+AndGrad@users.noreply.github.com>
2022-10-20 15:15:05 +0000

    updates and fixes
    scaled the sims to all participants
    made plots to visualizse behavior

--- B_SimulationCode/check_output_simulation.R
+++ B_SimulationCode/check_output_simulation.R
@@ -5,30 +5,45 @@
 
 explore_data <- read_csv(file = "Data/data_coord.csv")
 nogems <- subset(explore_data, gempresent == 0)
+                   
+                   
+ # plot next to behavioral data
+  group_by(player, env_number) %>% 
+  ggplot(aes(x = trial, y = points)) +
+  stat_summary() +
+  theme_minimal(base_size = 20)
 
-
-
-# select player number
+simd_data <- NULL
 
 for (n in 1:length(unique(explore_data$player))) {
-  
-  d_subset <- subset(explore_data, player == playerNr & gempresent == 0)
-  unique(d_subset$round)
-  
-    simd_data <-  readRDS(file = paste0("A_GeneratedFiles/simulations/simulated_choices", playerNr))
-  unique(simd_data$round)
-  
-  simd_data %>% 
-    group_by(round()) %>% 
-    ggplot +
-    geom_point( x = )
-  
+ one_player <-  readRDS(file = paste0("A_GeneratedFiles/simulations/simulated_choices", n))
+simd_data <- bind_rows(simd_data, one_player)
 }
+ 
+hist(simd_data$points_new_choice)
+ unique(simd_data$round)
 
-playerNr = 5
+ 
 
+# PLOT results------------------------------------------------------------------
 
+# points by round: should go up
+ 
+ simd_data %>% 
+   group_by(playerNr, env_counter) %>% 
+   ggplot(aes(x = trial, y = points_new_choice)) +
+   stat_summary() +
+   theme_minimal(base_size = 20)
 
 
 # read in simulated data
+ 
+ simd_data %>% 
+   group_by(playerNr, learning_rate) %>% 
+   summarise(tot_point = sum(points_new_choice)) %>% 
+   ggplot(aes(x = learning_rate, y = tot_point)) +
+   geom_point() +
+   theme_minimal(base_size = 20)
+ 
+ 
 
