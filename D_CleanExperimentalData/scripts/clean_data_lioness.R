4cb4eb4215b3c13f1dae4c65b7f7e31283d68742
Simon Remote <ciranka@mpib-berlin.mpg.de>
2023-05-31 17:20:16 +0200

    load demonstrators and modified social function

--- D_CleanExperimentalData/scripts/clean_data_lioness.R
+++ D_CleanExperimentalData/scripts/clean_data_lioness.R
@@ -7,73 +7,76 @@ library(dplyr)
 library(tibble)
 library(readxl)
 
-source('D_CleanExperimentalData/scripts/readLIONESS.R')
+source("D_CleanExperimentalData/scripts/readLIONESS.R")
 
-folder <- 'D_CleanExperimentalData/raw_data/lioness/'
-writinglocation <- 'D_CleanExperimentalData/raw_data'
-periods <- 12 # how many max periods in this 
+folder <- "D_CleanExperimentalData/raw_data/lioness/"
+writinglocation <- "D_CleanExperimentalData/raw_data"
+periods <- 12 # how many max periods in this
 
-# function input: 
+# function input:
 data <- readLIONESS(folder, writinglocation, periods)
 
 # calculate S and substitute playerNr with ID
 
 ## REPLACE WITH APPLY WHWN WILL BECOME BIG
 
-data_long <- data.frame(player = numeric(0),
-                        points = numeric(0),
-                        cells = numeric(0),
-                        round = numeric(0),
-                        env_number = numeric(0),
-                        gem_present = numeric(0),
-                        tot_points = numeric(0))
+data_long <- data.frame(
+  player = numeric(0),
+  points = numeric(0),
+  cells = numeric(0),
+  round = numeric(0),
+  env_number = numeric(0),
+  gem_present = numeric(0),
+  tot_points = numeric(0)
+)
 
-for (n in 1:length(unique(data$playerNr))){
-  
+for (n in 1:length(unique(data$playerNr))) {
   # create empty df
   playerMat <- data.frame()
-  
+
   # select current player
-  one_player_data <- data %>% 
+  one_player_data <- data %>%
     filter(playerNr == unique(data$playerNr)[n])
-  
 
-  for (t in 1:12){
 
-    points <-  as.numeric(unlist(str_split(one_player_data$points[t], ",")))
-    cells <-  as.numeric(unlist(str_split(one_player_data$clickedCells[t], ",")))
+  for (t in 1:12) {
+    points <- as.numeric(unlist(str_split(one_player_data$points[t], ",")))
+    cells <- as.numeric(unlist(str_split(one_player_data$clickedCells[t], ",")))
     env_number <- as.numeric(unlist(str_split(one_player_data$orderEnvs[1], ",")))[t]
     gempresent <- as.numeric(unlist(str_split(one_player_data$orderTreatment[1], ",")))[t]
-    
-    temp <- data.frame(player = rep(n, length(points)),
-     points = points,
-    cells = cells,
-    env_number = rep(env_number, length(points)),
-    gempresent = rep(gempresent, length(points)),
-    
-    round = rep(t, length(points)),
-    tot_points = rep(one_player_data$totalPoints[t], length(points)))
-    
+
+    temp <- data.frame(
+      player = rep(n, length(points)),
+      points = points,
+      cells = cells,
+      env_number = rep(env_number, length(points)),
+      gempresent = rep(gempresent, length(points)),
+      round = rep(t, length(points)),
+      tot_points = rep(one_player_data$totalPoints[t], length(points))
+    )
+
     playerMat <- rbind(playerMat, temp)
   }
-  
-    data_long <- rbind(data_long, playerMat)
+
+  data_long <- rbind(data_long, playerMat)
 }
 
 
 unique(data_long$tot_points)
 
 # save raw decisions dataset
-write.csv(data, 'D_CleanExperimentalData/raw_data/raw_data_decisions.csv', row.names = FALSE)
+write.csv(data, "D_CleanExperimentalData/raw_data/raw_data_decisions.csv", row.names = FALSE)
 
 
-data_long <- data_long %>% 
-  mutate(gem = ifelse(points > 200, 1, 0)) %>% 
+data_long <- data_long %>%
+  mutate(gem = ifelse(points > 200, 1, 0)) %>%
   group_by(player, round) %>%
-  mutate(trial = 1:25,
-         unique_rounds = cur_group_id()) 
+  mutate(
+    trial = 1:25,
+    unique_rounds = cur_group_id()
+  )
 
 
 # save clean dataset
 
-write.csv(data_long, 'D_CleanExperimentalData/clean_data/clean_data.csv', row.names = FALSE)
+write.csv(data_long, "D_CleanExperimentalData/clean_data/clean_data.csv", row.names = FALSE)
--- select_participants_app/global.R
+++ select_participants_app/global.R
@@ -1,64 +1,42 @@
-
 ### Load ----------------------------------------------------------------------
 # Load dataset and functions
 
-pacman::p_load(tidyverse,
-               plotly,
-               colorspace)
+pacman::p_load(
+  tidyverse,
+  plotly,
+  colorspace
+)
 
-`%!in%` = Negate(`%in%`)
+`%!in%` <- Negate(`%in%`)
 
 
-data_1 <-  as_tibble(read.csv('data_coord.csv'))
-#readRDS(paste0(here(),"/select_participants_app/explore_data.RDS"))
+data_1 <- as_tibble(read.csv("data_coord.csv"))
+# readRDS(paste0(here(),"/select_participants_app/explore_data.RDS"))
 
 make_plot <- function(dat, gem_present, gem_coords) {
-  
   gem <- gem_present
   data_round <- dat
   gem_coords_round <- gem_coords
-  
-  if(gem == 'yes'){
-  
-  plot <- 
-  ggplot() +
-    geom_rect(
-      data = gem_coords_round,
-      size = 1,
-      aes(
-        xmin = x - 1,
-        xmax = x  ,
-        ymin = y - 1,
-        ymax = y
-      ),   color = 'red',
-        fill = 'white',
-    ) +
-    geom_rect(
-      data = data_round,
-      aes(
-        xmin = x - 1,
-        xmax = x  ,
-        ymin = y - 1,
-        ymax = y,
-        fill = points,
-        frame = trial
-      ),
-      color = "black",
-      size = .2
-    ) +
-    scale_x_continuous(limits = c(-1, 7), breaks = -1:7) +
-    scale_y_continuous(limits = c(-1, 7), breaks = -1:7) +
-    scale_fill_continuous_divergingx(palette = "RdBu", mid = 0) 
-  }
-  
-  else if(gem == 'no'){
-    
-    plot <-  ggplot() +
+
+  if (gem == "yes") {
+    plot <-
+      ggplot() +
+      geom_rect(
+        data = gem_coords_round,
+        size = 1,
+        aes(
+          xmin = x - 1,
+          xmax = x,
+          ymin = y - 1,
+          ymax = y
+        ), color = "red",
+        fill = "white",
+      ) +
       geom_rect(
         data = data_round,
         aes(
           xmin = x - 1,
-          xmax = x  ,
+          xmax = x,
           ymin = y - 1,
           ymax = y,
           fill = points,
@@ -69,9 +47,26 @@ make_plot <- function(dat, gem_present, gem_coords) {
       ) +
       scale_x_continuous(limits = c(-1, 7), breaks = -1:7) +
       scale_y_continuous(limits = c(-1, 7), breaks = -1:7) +
-      scale_fill_continuous_divergingx(palette = "RdBu", mid = 0) 
+      scale_fill_continuous_divergingx(palette = "RdBu", mid = 0)
+  } else if (gem == "no") {
+    plot <- ggplot() +
+      geom_rect(
+        data = data_round,
+        aes(
+          xmin = x - 1,
+          xmax = x,
+          ymin = y - 1,
+          ymax = y,
+          fill = points,
+          frame = trial
+        ),
+        color = "black",
+        size = .2
+      ) +
+      scale_x_continuous(limits = c(-1, 7), breaks = -1:7) +
+      scale_y_continuous(limits = c(-1, 7), breaks = -1:7) +
+      scale_fill_continuous_divergingx(palette = "RdBu", mid = 0)
   }
- 
+
   return(plot)
-   
 }
