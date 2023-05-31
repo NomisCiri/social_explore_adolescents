4cc403a4c005371fc2fe3fde699f1f9ca152218a
Simon Remote <ciranka@mpib-berlin.mpg.de>
2023-05-31 16:01:51 +0200

    start putting social sims into simpler function

--- B_SimulationCode/Models.R
+++ B_SimulationCode/Models.R
@@ -193,10 +193,140 @@ exploreEnv<-function(explore_func,choiceRule,env2,env1,cntrl,iter){
 
 
 
+#########
+######### Exploration Environmental change
+#########
+exploreEnv<-function(explore_func,choiceRule,env2,env1,cntrl,iter){
+  #for (rep in 1:ntrialss){
+  #unpack
+  lambda=cntrl$lambda
+  #get beta
+  beta<-cntrl$beta# this scales risk attitude.
+  #get tau
+  tau<-cntrl$tau
+  mu0<-cntrl$mu0#exploration bonus
+  var0<-cntrl$var0
+  #create a parameter vector
+  parVec <- cntrl$parVec
+  #
+  ExploreBonus=cntrl$ExploreBonus
+  #kernel is RBF
+  #k<-rbf
+  #loop through trials
+  out=cntrl$out
+  AllChoices=cntrl$AllChoices
+  dummy=cntrl$dummy
+  overallCnt=cntrl$overallCnt
+  dat=cntrl$dat
+  mu=list()
+  sig=list()
+  
+  for (nround in 1:3){
+    #get parameters for participant on that round
+    if (nround==1){
+      # define vectors that are used by the kalman filter
+      lowestx=4
+      highestx=9
+      sampleVec=as.numeric(rownames(dat[dat$x1>=lowestx & dat$x1<=highestx  & dat$x2<7,]))# here you define where a child should sample from
+      ind<-sample(sampleVec,1)
+      nTrials=400
+    }else {
+      ind<-sample(1:64,1)
+      nTrials=400
+    }
+    #random initialization as observation t=0
+    #y matrix
+    if (nround==1 & overallCnt==1){
+      X<-as.matrix(dat[ind,1:2])# generate a new vector of Xs
+      y<-as.matrix(rnorm(1,mean = EnvirionemntAdol[ind,]$Mean,sd=EnvirionemntAdol[ind,]$Variance))
+    }else if(overallCnt==1) {
+      print("Youre an adolescent now")
+      X<-as.matrix(dat[ind,1:2])# generate a new vector of Xs
+      y<-as.matrix(rnorm(1,mean = EnvirionemntAdol[ind,]$Mean,sd=EnvirionemntAdol[ind,]$Variance))
+    }
+    #X-start, i.e. all possible observations
+    Xstar<-as.matrix(dat[,1:2])
+    
+    for (trial in 1:nTrials){
+      #output by GP with particular parameter settings
+      #don't forget mean centering and standardization.... mean is already 0 :)
+      if (overallCnt>1){
+        out<-bayesianMeanTracker(X[overallCnt,1:2],y[overallCnt], theta=lambda, prevPost=out,mu0Par=mu0,var0Par = var0)
+      }else{
+        out<-bayesianMeanTracker(X[overallCnt,1:2],y[overallCnt],theta=lambda, prevPost=NULL,mu0Par=mu0,var0Par=var0)
+      }
+      #browser()
+      #utility vector. transpose if you use greedyMean
+      #where is everybody?
+      #here i need a function that calls bayesianMeanTracker. n times and returns the values X for each n. Also, i need some kind of list, where i save the prior for each instance....
+      #
+      # browser()
+      utilityVec<-ucb(out,beta)
+      utilities <- utilityVec - max(utilityVec)
+      #softmaximization
+      p <- exp(utilities/tau)
+      #probabilities
+      p <- p/colSums(p)
+      #numerical overflow
+      p <- (pmax(p, 0.00001))
+      p <- (pmin(p, 0.99999))
+      #index is sampled proprotionally to softmaxed utitily vector
+      if (nround==1){# subset the probability vector so that it corresponds to the right tiles.
+        ind<-sample(sampleVec,1,prob=p[dat$x1>=lowestx & dat$x1<=highestx & dat$x2<7,])# sample from a childhood environemnt
+        #this monster just scales exploration boni
+      }else {
+        ind<-sample(1:64, 1, prob=p)# sample from an adolescent environemnt
+        # print(ind)
+      }
+      X<-rbind(X, as.matrix(dat[ind,1:2]))
+      #bind y-observations
+      y<-rbind(y, as.matrix(rnorm(n=1,mean = EnvirionemntAdol[ind,]$Mean,sd=EnvirionemntAdol[ind,]$Variance)))# change this into a sample.
+      #if(y[overallCnt]<0){
+      #  y[overallCnt]=-1*y[overallCnt]^2# make losses more severe. 
+      #}
+      
+      dummy<-data.frame(trial=overallCnt, x=as.numeric(X[overallCnt,1]), y=as.numeric(X[overallCnt,2]),
+                        z=as.numeric(y[overallCnt]),round=nround)
+      
+      AllChoices<-rbind(AllChoices,dummy)
+      mu[[overallCnt]]<-out$mu
+      sig[[overallCnt]]<-out$sig
+      overallCnt=overallCnt+1
+    }
+    #dummy data frame
+  }
+  #}
+  #This Here is for Plotting
+  
+  Plot_dat=expand.grid(x=1:8,y=1:8,trials=0:max(dummy$trial))
+  Plot_dat$sample=0
+  Plot_dat$out=0
+  Plot_dat$mu=0
+  Plot_dat$sig=40
+  
+  for (i in 1:length(AllChoices$x)){
+    AllChoices$y[i]
+    AllChoices$x[i]
+    Plot_dat[Plot_dat$x==AllChoices$x[i] & Plot_dat$y==AllChoices$y[i] & Plot_dat$trials==AllChoices$trial[i],]$sample=1
+    Plot_dat[Plot_dat$trials==AllChoices$trial[i],]$out=AllChoices$z[i]
+    Plot_dat[Plot_dat$trials==AllChoices$trial[i],]$mu=mu[[i]]
+    Plot_dat[Plot_dat$trials==AllChoices$trial[i],]$sig=sig[[i]]
+  }
+  # browser()
+  
+  Plot_dat$iter=iter
+  return(Plot_dat)
+}
+
+
+
+
+
+
 
 
 ####
-#### Social exploration
+#### Social exploration with recursion for ABM.
 ####
 WhereIsEverybody<-function(HowManyOthers,others,otherLoc,dat_social,X_oth,y_oth,diminishingSocial,trial,cntrl,sampleVec){
   lowestx=4
@@ -281,7 +411,9 @@ WhereIsEverybody<-function(HowManyOthers,others,otherLoc,dat_social,X_oth,y_oth,
 
 
 
-
+####
+#### wrapper function for recursive ABM.
+####
 
 
 exploreEnv_Social<-function(explore_func,choiceRule,socialfunc,env2,env1,cntrl,iter){
@@ -908,3 +1040,30 @@ modelSim <-
     
     return(choices_round)  
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
