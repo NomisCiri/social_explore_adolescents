91b02805130b26c01848e554c0a60632e12bff2c
Andrea Gradassi <82960682+AndGrad@users.noreply.github.com>
2023-07-10 14:24:10 +0200

    coding with Simon

--- D_CleanExperimentalData/scripts/prep_data_social.R
+++ D_CleanExperimentalData/scripts/prep_data_social.R
@@ -35,22 +35,33 @@ data$gem_found[is.na(data$gem_found)] <- 0
 
 
 
-data <- data %>% 
-  group_by(gempresent, gem_found) %>% 
+data <- data %>%
+  group_by(gempresent, gem_found) %>%
   mutate(performance_group = ntile(tot_points, 3)) %>%
   mutate(performance_group_f = ifelse(
     performance_group == 1,
     "low",
     ifelse(performance_group == 2, "medium",
-           "high"))) %>% 
-    ungroup() %>% 
-    mutate(social_info_type = case_when( soc_info_round %in% c(800,921,254,1599,1899,408,838) ~ 1,
-                           soc_info_round %in% c(1650, 504, 376, 868, 332, 1434, 1177, 1244, 468, 1639) ~ 2,
-                           soc_info_round %in% c(905, 1625, 1912, 335, 343, 795) ~ 3,
-                           TRUE ~ NaN ),
-           social_info_factor = ifelse(social_info_type == 1, "no gem", ifelse(social_info_type==2, "gem not found", ifelse(social_info_type == 3, "gem found", NA)))
-)
-
+           "high")
+  )) %>%
+  ungroup() %>%
+  mutate(
+    social_info_type = case_when(
+      soc_info_round %in% c(800, 921, 254, 1599, 1899, 408, 838) ~ 1,
+      soc_info_round %in% c(1650, 504, 376, 868, 332, 1434, 1177, 1244, 468, 1639) ~ 2,
+      soc_info_round %in% c(905, 1625, 1912, 335, 343, 795) ~ 3,
+      TRUE ~ NaN
+    ),
+    social_info_factor = ifelse(
+      social_info_type == 1,
+      "no gem",
+      ifelse(
+        social_info_type == 2,
+        "gem not found",
+        ifelse(social_info_type == 3, "gem found", NA)
+      )
+    )
+  )
 
 
 
