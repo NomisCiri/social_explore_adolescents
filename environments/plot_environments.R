cb66bbea195c2256936b9d10c02936f2e6d63500
Andrea Gradassi <82960682+AndGrad@users.noreply.github.com>
2024-02-13 19:20:09 +0100

    udpates to plots

--- environments/plot_environments.R
+++ environments/plot_environments.R
@@ -29,7 +29,7 @@ breaks <- c(breaks, Inf)
 ## cut data
 envs$y_bin <- cut(envs$Mean, breaks = breaks, include.lowest = TRUE, labels = c("-50", "-35", "-7","0", "7", "35", "50", "240+ (gem)"))
 
-
+## plot everything at once
 environments <- 
 ggplot() +
   geom_tile(data = envs, aes(x = x, y = y, fill = as.factor(y_bin))) +
@@ -43,6 +43,31 @@ ggplot() +
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         aspect.ratio = 1)
-  
 
 ggsave(environments, filename = "plots/environments.png")
+
+## plot environments one by one
+for (e in unique(envs$env)) {
+  
+  ## select environment
+  d <- envs %>% dplyr::filter(env == e)
+  
+  ## make plot
+  plot <- 
+  ggplot() +
+    geom_tile(data = d, aes(x = x, y = y, fill = as.factor(y_bin))) +
+    scale_fill_manual(values = colors, name = "Tile mean value") +
+    theme_minimal(14) +
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(), 
+          axis.text.x = element_blank(), 
+          axis.text.y = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          aspect.ratio = 1) +
+    guides(fill = FALSE)
+  
+  ## save plot
+  ggsave(plot, filename = paste0("plots/environment", e, ".png"))
+
+}
