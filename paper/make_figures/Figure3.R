88d580d8dd5e3d142035422287b7da6f7d48b109
Andrea Gradassi <82960682+AndGrad@users.noreply.github.com>
2024-07-10 19:59:27 +0200

    updates

--- paper/make_figures/Figure3.R
+++ paper/make_figures/Figure3.R
@@ -16,7 +16,7 @@ pacman::p_load(tidyverse, gghalves, here, lmerTest, ggthemes, cowplot)
 all_data <- read_csv(file = paste0(here(), "/data/social/data_social_all_participants.csv")) 
 
 ## panel 1 data
-load(file = paste0(here(),'/G_Analysis_bevioral_data_social/modelfits/performance_age_demonstrator_model.RData'))
+load(file = paste0(here(),'/G_Analysis_bevioral_data_social/modelfits/performance_age_demonstrator_model_scaled.RData'))
     
 ## panel 2 data
 load(file = paste0(here(),'/G_Analysis_bevioral_data_social/modelfits/gem_found_when_model.RData'))
@@ -30,7 +30,7 @@ load(file = paste0(here(),'/G_Analysis_bevioral_data_social/modelfits/poission_r
 ## panel 1
 panel_plot1 <- 
   plot_model(
-    performance_age_demonstrator_model,
+    performance_age_demonstrator_model_scaled,
     # axis.lim = c(.2, 2),
     axis.labels = rev(
       c(
@@ -41,10 +41,15 @@ panel_plot1 <-
         "Quality (Worst) X Adolescents"
       )
     ),
-    title = "", vline.color = "grey", vline = 2,show.values = TRUE, 
+    title = "Points earned (std)", vline.color = "grey", vline = 2,show.values = TRUE, 
   ) +
+  labs(tag = "a")+
   theme_base(base_size = 15)+
-  theme(plot.background = element_blank())
+  theme(plot.background = element_blank(),
+        plot.title = element_text(size=15, hjust = 0.5),
+        plot.tag.position = c(0,1),
+        plot.tag = element_text(hjust = -9)
+        )
 
 panel_plot1
 
@@ -62,32 +67,43 @@ panel_plot2 <-
         "Quality (Worst) X Adolescents"
       )
     ),
-    title = "", vline.color = "grey", vline = 2,show.values = TRUE, 
+    title = "N of copy decisions", 
+    vline.color = "grey", vline = 2,
+    show.values = TRUE, 
   ) +
+  labs(tag = "b")+
   ylim(.2,2)+
   theme_base(base_size = 15)+
   theme(plot.background = element_blank())+
   theme(axis.title.y=element_blank(),
         axis.text.y=element_blank(),
-        axis.ticks.y=element_blank())
+        axis.ticks.y=element_blank(),
+        plot.title = element_text(size=15, hjust = 0.5),
+        plot.tag.position = c(0,1),
+  )
 
 
 panel_plot2
 
 
-## panel 2
+## panel 3
 panel_plot3 <- 
   plot_model(
     gem_found_when_model
     ,
-    title = "", vline.color = "grey", vline = 2,show.values = TRUE, 
+    title = "N of clicks to find a gem", 
+    vline.color = "grey", vline = 2,show.values = TRUE, 
   ) +
   ylim(.2,2)+
+  labs(tag = "c")+
   theme_base(base_size = 15)+
   theme(plot.background = element_blank()) +
   theme(axis.title.y=element_blank(),
         axis.text.y=element_blank(),
-        axis.ticks.y=element_blank())
+        axis.ticks.y=element_blank(),
+        plot.title = element_text(size=15, hjust = 0.5),
+        plot.tag.position = c(0,1),
+  )
 
 panel_plot3
 
@@ -96,7 +112,8 @@ panel_plot3
 figure3 <- 
   cowplot::plot_grid(
     panel_plot1, panel_plot2, panel_plot3,
-    # labels = c("","c"),
+   # labels = c("a", "b","c"),
+    label_x = -.01,
     #align = "H",
     nrow = 1,
     rel_widths =  c(.95, .65, .65)
